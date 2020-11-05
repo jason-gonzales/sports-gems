565c6f8b84b628f4cd49aa73c75cf5ed38c1b895
Jason Gonzales <jasongonzo25@gmail.com>
2020-11-05 13:13:22 -0800

    completed add to cart front end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,13 +10,16 @@ export default class App extends React.Component {
       view: {
         name: 'catalog',
         params: {}
-      }
+      },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.getCartItems = this.getCartItems.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
   componentDidMount() {
-
+    this.getCartItems();
   }
 
   setView(name, params) {
@@ -28,13 +31,39 @@ export default class App extends React.Component {
     });
   }
 
+  getCartItems() {
+    fetch('api/cart')
+      .then(res => res.json())
+      .then(cart => {
+        this.setState({ cart: cart });
+      })
+      .catch(err => console.error(err));
+  }
+
+  addToCart(product) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(product)
+    };
+    fetch('api/cart', requestOptions)
+      .then(result => result.json())
+      .then(data => {
+        const updateCart = this.state.cart.slice();
+        updateCart.push(data);
+        this.setState({ cart: updateCart });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
+
     if (this.state.view.name === 'catalog') {
       return (
         <>
-          <Header/>
+          <Header cartCount ={this.state.cart.length}/>
           <ProductList
-            setView={this.setView}/>
+            setView={this.setView} />
         </>
       );
     }
@@ -42,10 +71,11 @@ export default class App extends React.Component {
     if (this.state.view.name === 'details') {
       return (
         <>
-          <Header/>
+          <Header cartCount = {this.state.cart.length}/>
           <ProductDetails
             setView={this.setView}
-            params={this.state.view.params}/>
+            params={this.state.view.params}
+            addToCart={this.addToCart} />
         </>
       );
     }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
- function Header(props) {
+function Header(props) {
 
   return (
     <header className="mb-30 mt-0 d-flex align-items-center">
       <h2 className="ml-5"><i className="fas fa-dollar-sign"></i>Wicked Sales</h2>
+      <div className ="cart row justify-content-end col-9 align-items-center">
+        <p className ="m-0">{props.cartCount} Items</p>
+        <i className ="fas fa-shopping-cart"></i>
+      </div>
     </header>
   );
 }
