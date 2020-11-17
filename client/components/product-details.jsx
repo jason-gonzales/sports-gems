8bd85dbb46463862ade51c506a98491dc148850d
Jason Gonzales <jasongonzo25@gmail.com>
2020-11-16 22:40:47 -0800

    work in progress

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -44,26 +44,28 @@ export default class ProductDetails extends React.Component {
   render() {
     if (this.state.product) {
       return (
-        <div className="container" onClick={this.handleClick}>
-          <div className="d-flex justify-content-center">
-            <div className="card card-detail-product">
-              <div className="row ml-5 mb-3" id="catalog">Back to catalog</div>
-              <div className="row no-gutters">
-                <div className="col-6">
-                  <img src={this.state.product.image} className="card-img" alt={this.state.product.name}/>
-                </div>
-                <div className="shake col-md-6">
-                  <div className="card-body">
-                    <h5 className="card-title">{this.state.product.name}</h5>
-                    <p>${this.priceFormat()}</p>
-                    <p className="card-text">{this.state.product.shortDescription}</p>
-                    <button className ="btn btn-primary" onClick={this.handleCart}>Add To Cart</button>
+        <div className="product-detail-bg">
+          <div className="container" onClick={this.handleClick}>
+            <div className="d-flex justify-content-center">
+              <div className="card card-detail-product">
+                <div className="row ml-5 mb-3" id="catalog">Back to catalog</div>
+                <div className="row no-gutters">
+                  <div className="col-6">
+                    <img src={this.state.product.image} className="card-img" alt={this.state.product.name}/>
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-12">
-                    <p className="card-text col-12 my-4">{this.state.product.longDescription}</p>
+                  <div className="shake col-md-6">
+                    <div className="card-body">
+                      <h5 className="card-title">{this.state.product.name}</h5>
+                      <p>${this.priceFormat()}</p>
+                      <p className="card-text">{this.state.product.shortDescription}</p>
+                      <button className ="btn btn-primary" onClick={this.handleCart}>Add To Cart</button>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-12">
+                      <p className="card-text col-12 my-4">{this.state.product.longDescription}</p>
 
+                    </div>
                   </div>
                 </div>
               </div>
