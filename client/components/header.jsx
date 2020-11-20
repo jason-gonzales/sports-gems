f5ca07cd9beb5671706babb3b698e6ba94355d44
Jason Gonzales <jasongonzo25@gmail.com>
2020-11-20 09:51:50 -0800

    completed

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,7 +7,7 @@ function Header(props) {
   return (
     <header className="sticky-top">
       <div className="pt-2 d-flex">
-        <h2 className="col head-name" onClick={() => props.setView('catalog', null)} ><img className="trophy" src="/images/trophy-logo.png"/>Sports Treasures</h2>
+        <h2 className="col head-name" onClick={() => props.setView('catalog', null)} ><img className="trophy" src="/images/trophy-logo.png"/>Sports Gems</h2>
         <div className="d-flex align-items-center pb-2">
           <p className ="m-0 cart">{props.cartCount} Items</p>
           <i className ="cart fas fa-shopping-cart mr-4 pl-2" onClick={handleClick}></i>
